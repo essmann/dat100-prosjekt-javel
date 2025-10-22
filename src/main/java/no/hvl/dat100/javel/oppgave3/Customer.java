550be634f2e4acdf3807a8b3e6fa9681c399f688
Essmann <kenwilliam1000@gmail.com>
2025-10-22 13:51:54 +0200

    Alle oppgavene I oppgave 3 gjort.

--- src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/Customer.java
@@ -4,13 +4,38 @@ public class Customer {
 
     // TODO - object variables (attributes)
 
+    String email;
+    String name;
+    PowerAgreementType agreement;
+    int customer_id;
+
     public Customer(String name, String email, int customer_id, PowerAgreementType agreement) {
 
+        this.name = name;
+        this.email = email;
+        this.customer_id = customer_id;
+        this.agreement = agreement;
         // TODO
+
     }
 
     // TODO - getter/setter methods for all objectvariables
-
+    String getEmail(){
+        return this.email;
+    }
+    String getName(){
+        return this.name;
+    }
+    PowerAgreementType getAgreement(){
+        return this.agreement;
+    }
+    int getCustomerId(){
+        return this.customer_id;
+    }
     // TODO - toString method
+    @Override public String toString(){
+        String str = " Customer number %d \n Name %s \n Email %s \n Agreement %s ";
+        return String.format(str, customer_id, name, email, agreement);
+    }
 
 }
--- src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
+++ src/main/java/no/hvl/dat100/javel/oppgave3/CustomerMain.java
@@ -8,6 +8,15 @@ public class CustomerMain {
 
         System.out.println("==============");
         System.out.println("OPPGAVE 3");
+        Customer customer = new Customer("Ken", "something@gmail.com", 123, PowerAgreementType.SPOTPRICE);
+        System.out.println("Testing methods... e)");
+        System.out.println("d) toString method: ");
+        System.out.println(customer.toString());
+        System.out.println("Testing get/set methods c)");
+        System.out.println("getName(): " + customer.getName() + " getEmail(): " + customer.getEmail());
+        System.out.println("getCustomerId(): " + customer.getCustomerId() + " getAgreement(): " + customer.getAgreement());
+
+        
         System.out.println("==============");
         System.out.println();
 
